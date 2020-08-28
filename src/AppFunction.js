c262aa795e4c5191ad0fb4d7c1984e20f862c70e
stefian <v_ianta@yahoo.com>
2020-08-28 13:29:44 -0300

    + const [isOn, setIsOn] = useState(false)

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -2,15 +2,34 @@ import React, {useState} from 'react'
 
 const App = () => {
   const [count, setCount] = useState(0)
+  const [isOn, setIsOn] = useState(false)
 
   const incrementCount = () => {
     setCount(prevCount => prevCount + 1)
   }
 
-  return <button onClick={incrementCount}>
+  const toggleLight = () => {
+    setIsOn(prevIsOn => !prevIsOn)
+  }
+
+  return (
+    <>
+    <h2>Counter</h2>
+    <button onClick={incrementCount}>
     I was clicked {count} times
-  </button>
-  
+    </button>
+      
+    <h2>Toggle Light</h2>
+    <div
+      style={{
+        height: '50px',
+        width: '50px',
+        background: isOn ? 'yellow' : 'grey'
+      }}
+      onClick={toggleLight}
+    ></div>
+    </>
+  )
 }
 
 export default App;
