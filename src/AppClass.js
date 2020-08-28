42c1f31c6a2820a5851c35e9af316037b6cd83bb
stefian <v_ianta@yahoo.com>
2020-08-28 13:20:40 -0300

    +   toggleLight = () => {     this.setState(prevState => ({       isOn: !prevState.isOn     }))   }

--- src/AppClass.js
+++ src/AppClass.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 
 class App extends Component {
   state = {
-    count: 0
+    count: 0,
+    isOn: false
   }
 
   incrementCount = () => {
@@ -11,9 +12,28 @@ class App extends Component {
     }))
   }
 
+  toggleLight = () => {
+    this.setState(prevState => ({
+      isOn: !prevState.isOn
+    }))
+  }
+
   render() {
-  return (
-    <button onClick={this.incrementCount}>I was clicked {this.state.count} times</button>
+    return (
+      <>
+      <h2>Counter</h2> 
+        <button onClick={this.incrementCount}>I was clicked {this.state.count} times</button>
+        
+        <h2>Toggle Light</h2>
+        <div
+          style={{
+            height: '50px',
+            width: '50px',
+            background: this.state.isOn ? 'yellow' : 'grey'
+          }}
+          onClick={this.toggleLight}
+        ></div>
+      </>
   );
   }
 }
