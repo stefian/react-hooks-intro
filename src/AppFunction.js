34ab5ad85e8fa32b89700000eb84a0df48d4b3d1
stefian <v_ianta@yahoo.com>
2020-08-28 13:07:53 -0300

    +   const incrementCount = () => {     setCount(prevCount => prevCount + 1)   }

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -4,7 +4,7 @@ const App = () => {
   const [count, setCount] = useState(0)
 
   const incrementCount = () => {
-    setCount(count + 1)
+    setCount(prevCount => prevCount + 1)
   }
 
   return <button onClick={incrementCount}>
